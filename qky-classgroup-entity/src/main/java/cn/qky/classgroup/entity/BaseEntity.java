a1c24efc7126460559b4740132faf9ba7109c43b
Edwin <edwin13@qq.com>
2019-01-09 18:37:25 +0800

    添加flyway

--- qky-classgroup-entity/src/main/java/cn/qky/classgroup/entity/BaseEntity.java
+++ qky-classgroup-entity/src/main/java/cn/qky/classgroup/entity/BaseEntity.java
@@ -19,7 +19,7 @@ import java.io.Serializable;
 @Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "fieldHandler","DTYPE"})
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "fieldHandler"})
 public abstract class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
